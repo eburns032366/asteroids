9bd1ccd14f1657353ce0834feb813eeed701197a
eburns <eburns@coolidgeroad.us>
2025-10-14 15:24:01 -0400

    End of chapter 2

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    clock_obj = pygame.time.Clock()
-    dt = 0
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
+
+        updatable.update(dt)
+
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
-        dt = clock_obj.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
